a0cb1859796975285d09f1b6163a3f951a53150e
Daniel Barney <daniel@thoughtplot.io>
2023-09-15 17:03:31 +0000

    got the probability encoded. now need to build the override

--- main.go
+++ main.go
@@ -4,175 +4,124 @@ import (
 	"bufio"
 	"cmp"
 	"encoding/binary"
-	"encoding/xml"
 	"errors"
 	"fmt"
 	"io"
 	"os"
-	"reflect"
 	"slices"
 )
 
 type node struct {
-	value      string
-	compressed uint32
-	order      uint64
-	count      uint64
+	value   uint32
+	next    map[byte]uint32
+	ordered []byte
 }
 
-/*
-  <page>
-    <title>AaA</title>
-    <id>1</id>
-    <revision>
-      <id>32899315</id>
-      <timestamp>2005-12-27T18:46:47Z</timestamp>
-      <contributor>
-        <username>Jsmethers</username>        <id>614213</id>
-      </contributor>
-      <text xml:space="preserve">#REDIRECT [[AAA]]</text>
-    </revision>
-  </page>
-*/
-type page struct {
-	title string
-	id    uint32
-	rev_id
-	rev_ts
-	rev_user
-	rev_user_id
-	text []byte
-}
-
-func (p *page) String() string {
-	return "page"
-}
-
-func parsePage(x *xml.Decoder) *page {
-	p := &page{}
-	for {
-		t, err := x.Token()
-		if err != nil {
-			panic(err)
-		}
-		switch elem := t.(type) {
-		case xml.EndElement:
-		case xml.CharData:
-		case xml.StartElement:
-			v, err := x.Token()
-			if err != nil {
-				panic(err)
-			}
-			switch elem.Name.Local {
-			case "title":
-				p.title = string(v)
-			case "id":
-				p.title = strconv.Atoi(string(v))
-			case "revision":
-				parseRevision(x, p)
-			case "text":
-				p.text = v
-			}
-		}
-	}
-	return p
-}
-
-func parseRevision(x *xml.Decoder, p *page) {
-	for {
-		t, err := x.Token()
-		if err != nil {
-			panic(err)
-		}
-		switch elem := t.(type) {
-		case xml.EndElement:
-		case xml.CharData:
-		case xml.StartElement:
-	
-}
 func main() {
-	f, err := os.Open("enwik9")
+	f, err := os.Open(os.Args[1])
 	if err != nil {
 		panic(err)
 	}
-	x := xml.NewDecoder(f)
-
-	for i := 0; i < 1000; i++ {
-		t, err := x.Token()
-		if err != nil {
-			panic(err)
-		}
-		switch elem := t.(type) {
-		case xml.EndElement:
-		case xml.CharData:
-		case xml.StartElement:
-			if elem.Name.Local == "page" {
-				p := parsePage(x)
-				fmt.Println(p)
-				return
-			}
-		default:
-			fmt.Println(reflect.TypeOf(t))
-		}
-	}
-	return
 	br := bufio.NewReader(f)
-	all := map[string]*node{}
-	order := []*node{}
+	all := map[uint32]*node{}
 	length := 4
-	amount := 1024 / length
-	for i := 0; i < amount; i++ {
-		slice := make([]byte, length)
-		_, err := io.ReadFull(br, slice)
+	slice := make([]byte, length)
+	_, err = io.ReadFull(br, slice)
+	if err != nil {
+		panic(err)
+	}
+	key := binary.BigEndian.Uint32(slice)
+	current := &node{
+		next: map[byte]uint32{},
+	}
+	all[key] = current
+	first := key
+	i := 0
+	for {
+		b, err := br.ReadByte()
 		if err != nil && !errors.Is(err, io.EOF) {
 			panic(err)
 		}
-		key := string(slice)
+		current.next[b]++
+		slice = append(slice[1:], b)
+		key := binary.BigEndian.Uint32(slice)
 		next, ok := all[key]
 		if !ok {
 			next = &node{
-				value:      key,
-				compressed: binary.LittleEndian.Uint32(slice),
-				count:      1,
-				order:      uint64(i),
+				next: map[byte]uint32{},
 			}
-			all[next.value] = next
-		} else {
-			next.count++
+			all[key] = next
 		}
-		order = append(order, next)
-		fmt.Printf("\rkeys %v processed:%v", len(all), i*4)
+		current = next
+		if i%1000 == 0 {
+			fmt.Printf("\rkeys %v processed:%v", len(all), i)
+		}
+		i++
 		if err == io.EOF {
 			break
 		}
 	}
+
 	fmt.Println("")
+	fmt.Println("ordering bytes")
+	for _, node := range all {
+		ord := []byte{}
+		for k := range node.next {
+			ord = append(ord, k)
+		}
+		// swap args, we want decending order
+		slices.SortFunc(ord, func(a, b byte) int {
+			if node.next[b] == node.next[a] {
+				return cmp.Compare(b, a)
+			}
+			return cmp.Compare(node.next[b], node.next[a])
+		})
+		node.ordered = ord
+	}
+	fmt.Println("building diff")
+
 	fmt.Println("summary:")
 	fmt.Println("key size: ", len(all)*length)
 	fmt.Println("unique: ", len(all))
+	fmt.Println("example:")
 
-	count := 0
-	for _, n := range order {
-		if n.count == 1 {
-			count++
-		} else if count != 0 {
-			fmt.Println("run of ", count)
-			count = 0
-		}
+	buf := []byte{0, 0, 0, 0}
+	binary.BigEndian.PutUint32(buf, first)
+	fmt.Printf("%v", string(buf))
+	for i = 0; i < 100; i++ {
+		key := binary.BigEndian.Uint32(buf)
+		node := all[key]
+		b := node.ordered[0]
+		buf = append(buf[1:], b)
+		fmt.Printf("%v", string([]byte{b}))
 	}
 
-	slices.SortFunc(order, func(a, b *node) int {
-		if a.count == b.count {
-			return cmp.Compare(a.value, b.value)
+	kf, err := os.OpenFile(os.Args[1]+".shrink", os.O_CREATE|os.O_TRUNC|os.O_RDWR, 0755)
+	if err != nil {
+		panic(err)
+	}
+	ordered := []uint32{}
+	for k := range all {
+		ordered = append(ordered, k)
+	}
+	slices.Sort(ordered)
+	prev := uint32(0)
+	for _, key := range ordered {
+		node := all[key]
+		buf := []byte{0, 0, 0, 0, 0, 0, 0, 0}
+		out := []byte{}
+		n := binary.PutUvarint(buf, uint64(key-prev))
+		out = append(out, buf[:n]...)
+		n = binary.PutUvarint(buf, uint64(len(node.next)))
+		out = append(out, buf[:n]...)
+		prev = key
+		for c := range node.next {
+			out = append(out, c)
+			// no uvarint as it makes it bigger
 		}
-		return cmp.Compare(a.count, b.count)
-	})
-	prev := order[0]
-	fmt.Println(prev.value, prev.count)
-	for _, n := range order {
-		if n.value != prev.value {
-			fmt.Println(n.value, n.count)
-			prev = n
+		_, err = kf.Write(buf)
+		if err != nil {
+			panic(err)
 		}
 	}
 }
