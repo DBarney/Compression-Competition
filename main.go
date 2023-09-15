50b87eaaba3e599f40077e97599afceca938bd21
Daniel Barney <daniel@thoughtplot.io>
2023-09-15 13:13:59 +0000

    switching gears, no special xml encoding

--- main.go
+++ main.go
@@ -4,10 +4,12 @@ import (
 	"bufio"
 	"cmp"
 	"encoding/binary"
+	"encoding/xml"
 	"errors"
 	"fmt"
 	"io"
 	"os"
+	"reflect"
 	"slices"
 )
 
@@ -18,11 +20,102 @@ type node struct {
 	count      uint64
 }
 
+/*
+  <page>
+    <title>AaA</title>
+    <id>1</id>
+    <revision>
+      <id>32899315</id>
+      <timestamp>2005-12-27T18:46:47Z</timestamp>
+      <contributor>
+        <username>Jsmethers</username>        <id>614213</id>
+      </contributor>
+      <text xml:space="preserve">#REDIRECT [[AAA]]</text>
+    </revision>
+  </page>
+*/
+type page struct {
+	title string
+	id    uint32
+	rev_id
+	rev_ts
+	rev_user
+	rev_user_id
+	text []byte
+}
+
+func (p *page) String() string {
+	return "page"
+}
+
+func parsePage(x *xml.Decoder) *page {
+	p := &page{}
+	for {
+		t, err := x.Token()
+		if err != nil {
+			panic(err)
+		}
+		switch elem := t.(type) {
+		case xml.EndElement:
+		case xml.CharData:
+		case xml.StartElement:
+			v, err := x.Token()
+			if err != nil {
+				panic(err)
+			}
+			switch elem.Name.Local {
+			case "title":
+				p.title = string(v)
+			case "id":
+				p.title = strconv.Atoi(string(v))
+			case "revision":
+				parseRevision(x, p)
+			case "text":
+				p.text = v
+			}
+		}
+	}
+	return p
+}
+
+func parseRevision(x *xml.Decoder, p *page) {
+	for {
+		t, err := x.Token()
+		if err != nil {
+			panic(err)
+		}
+		switch elem := t.(type) {
+		case xml.EndElement:
+		case xml.CharData:
+		case xml.StartElement:
+	
+}
 func main() {
 	f, err := os.Open("enwik9")
 	if err != nil {
 		panic(err)
 	}
+	x := xml.NewDecoder(f)
+
+	for i := 0; i < 1000; i++ {
+		t, err := x.Token()
+		if err != nil {
+			panic(err)
+		}
+		switch elem := t.(type) {
+		case xml.EndElement:
+		case xml.CharData:
+		case xml.StartElement:
+			if elem.Name.Local == "page" {
+				p := parsePage(x)
+				fmt.Println(p)
+				return
+			}
+		default:
+			fmt.Println(reflect.TypeOf(t))
+		}
+	}
+	return
 	br := bufio.NewReader(f)
 	all := map[string]*node{}
 	order := []*node{}
