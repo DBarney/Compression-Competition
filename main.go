cd49fa263e58422060c29c10a7763d9af0edff34
Daniel Barney <daniel@thoughtplot.io>
2023-12-22 06:36:08 +0000

    switching again

--- main.go
+++ main.go
@@ -30,6 +30,7 @@ func update(pattern string, values ...interface{}) {
 }
 
 type file struct {
+	Length       int
 	Words        []string
 	Count        map[string]int
 	CountOrder   []string
@@ -121,9 +122,12 @@ func loadFile(name string) (*file, error) {
 		for _, w := range commonWords {
 			common[w] = true
 		}
-		re := regexp.MustCompile(`('&|[[|<)?([\w-]+|[^\w]+)(]])?[> ,.;:|*\n-]*`)
+		//re := regexp.MustCompile(`('&|[[|<)?([\w-]+|[^\w]+)(]])?[> ,.;:|*\n-]*`)
+		//re := regexp.MustCompile(`([&\[{'"]+)?([\w-]+|[^\w]+)([\]}'"]+)?[,.;:|*-]*[ ]+`)
+		re := regexp.MustCompile(`([\w-]+|[^\w]+)[ ]*`)
 		chunks := re.FindAll(file, -1)
 		fmt.Println("processing chunks")
+		meta.Length = len(file)
 		meta.Words = []string{""}
 		meta.Count = map[string]int{}
 		meta.Locations = map[string][]int{}
@@ -131,11 +135,13 @@ func loadFile(name string) (*file, error) {
 		prev := ""
 		for i, v := range chunks {
 			token := string(v)
-			token = strings.ToLower(token)
-			token = strings.Trim(token, "<>[] .,;:-|\n")
-			if common[token] {
-				continue
-			}
+			/*
+				token = strings.ToLower(token)
+					token = strings.Trim(token, "<>[] .,;:-|\n")
+					if common[token] {
+						//	continue
+					}
+			*/
 			update("%05.2f converting to strings\r", float32(i)/float32(len(chunks))*100)
 			meta.Words = append(meta.Words, token)
 			meta.Count[token]++
@@ -187,6 +193,7 @@ func loadFile(name string) (*file, error) {
 		if err != nil {
 			return nil, err
 		}
+		fmt.Println("")
 	}
 	return meta, nil
 }
@@ -581,20 +588,479 @@ func main() {
 	if err != nil {
 		panic(err)
 	}
-	build(meta)
+	//build(meta)
+	graph(meta)
 }
 
 func build(meta *file) {
-	for _, k := range meta.CountOrder {
-		fmt.Println(k, meta.Count[k])
-		if meta.Count[k] < 100 {
-			break
-		}
-	}
-	fmt.Println("number of unique tokens", len(meta.Unique))
 	total := 0
 	for _, c := range meta.Count {
 		total += c
 	}
+
+	running := 0.0
+	count := 1
+	common := map[string]bool{}
+	uncommonSize := 0
+	uniqueSize := 0
+	commonBuffer := []byte{}
+	commonPBuffer := []byte{}
+	uniqueBuffer := []byte{}
+	uniquePBuffer := []byte{}
+	uncommonBuffer := []byte{}
+	uncommonPBuffer := []byte{}
+	for _, k := range meta.CountOrder {
+		percent := float64(meta.Count[k]) / float64(total) * 100
+		running += percent
+		if running < 45.0 {
+
+			common[k] = true
+		} else if meta.Count[k] > 1 {
+			fmt.Printf("%v\t%v\t%.2f\t%.2f\t%v\n", count, meta.Count[k], percent, running, k)
+			// 3 bytes to encode string
+			uncommonSize += len(k) + 4
+			uncommonBuffer = append(uncommonBuffer, []byte(k)...)
+			// one byte for null terminated string
+			uncommonBuffer = append(uncommonBuffer, '0')
+			for i := 0; i < meta.Count[k]; i++ {
+				// every reference adds a 3 byte key
+				uncommonPBuffer = append(uncommonPBuffer, '0', '0', '0')
+			}
+
+		} else {
+			// length of string, no dups
+			uniqueSize += len(k) + 1
+			uniqueBuffer = append(uniqueBuffer, []byte(k)...)
+			// just a null terminated string
+			uniqueBuffer = append(uniqueBuffer, '0')
+			// every reference adds a 3 byte key
+			uniquePBuffer = append(uniquePBuffer, '0', '0', '0')
+		}
+		//		fmt.Printf("%v\t%v\t%.2f\t%.2f\t%v\n", count, meta.Count[k], percent, running, k)
+		count++
+	}
+
+	cur := 0
+	max := 0
+	dist := map[int]int{}
+	prefixes := map[string]int{}
+	prefix := []string{}
+	for _, w := range meta.Words {
+		if common[w] {
+			cur++
+			prefix = append(prefix, w)
+		} else {
+			if max < cur {
+				max = cur
+			}
+			if cur > 1 {
+				prefixes[strings.Join(prefix, "")]++
+			}
+			prefix = []string{}
+			dist[cur]++
+			cur = 0
+		}
+	}
+	fmt.Println("")
+	for i := 0; i <= max; i++ {
+		//		fmt.Printf("%v\t%v\n", i, dist[i])
+	}
+	keys := []int{}
+	rev := map[int][]string{}
+	for k, v := range prefixes {
+		rev[v] = append(rev[v], k)
+	}
+	for k := range rev {
+		keys = append(keys, k)
+	}
+	slices.Sort(keys)
+	slices.Reverse(keys)
+	keys = slices.Compact(keys)
+
+	size := 0
+	commonSize := 0
+	for i, k := range keys {
+		for _, v := range rev[k] {
+			//			fmt.Printf("%v\t%v\n", k, v)
+			size += len(v) * k
+			// null terminate the string
+			commonBuffer = append(commonBuffer, []byte(v)...)
+			commonBuffer = append(commonBuffer, '0')
+			if i <= 127 {
+				// one byte per reference
+				for j := 0; j < k; j++ {
+					commonPBuffer = append(commonPBuffer, '0')
+				}
+				commonSize += k
+			} else if i < 32768 {
+				// two byte per reference
+				for j := 0; j < k; j++ {
+					commonPBuffer = append(commonPBuffer, '0', '0')
+				}
+				commonSize += 2 * k
+			} else {
+				// three byte per reference
+				for j := 0; j < k; j++ {
+					commonPBuffer = append(commonPBuffer, '0', '0', '0')
+				}
+				commonSize += 3 * k
+			}
+		}
+	}
+
+	once := 0
+	for _, v := range meta.Count {
+		if v == 1 {
+			once++
+		}
+	}
+	fmt.Println("unique runs of common", len(prefixes))
+	fmt.Println("common token compression ratio", size, commonSize)
+	fmt.Println("common tokens", len(common))
+	fmt.Println("uncommon tokens", len(meta.Unique)-len(common))
+	fmt.Println("max common token length", max)
+	fmt.Println("number of unique tokens", len(meta.Unique))
+	fmt.Println("tokens used once", once)
 	fmt.Println("total tokens", total)
+	fmt.Println("")
+
+	fmt.Printf("commonSize\t%v\t%v\t%v\n", commonSize, len(commonBuffer), len(commonPBuffer))
+	fmt.Printf("uncommonSize\t%v\t%v\t%v\n", uncommonSize, len(uncommonBuffer), len(uncommonPBuffer))
+	fmt.Printf("uniqueSize\t%v\t%v\t%v\n", uniqueSize, len(uniqueBuffer), len(uniquePBuffer))
+	fmt.Printf("compression ratio\t%.2f\n", float64(commonSize+uncommonSize+uniqueSize)/float64(meta.Length)*100)
+	fmt.Printf("buffer compression ratio\t%.2f\n", float64(len(commonPBuffer)+len(commonBuffer)+len(uncommonPBuffer)+len(uncommonBuffer)+len(uniquePBuffer)+len(uniqueBuffer))/float64(meta.Length)*100)
+	fmt.Printf("file size\t%v\n", meta.Length)
+}
+
+func graph(meta *file) {
+	total := 0
+	for _, c := range meta.Count {
+		total += c
+	}
+
+	running := 0.0
+	common := map[string]float64{}
+	for _, k := range meta.CountOrder {
+		percent := float64(meta.Count[k]) / float64(total) * 100
+		running += percent
+		if running < 50.0 {
+			common[k] = percent * 100
+			//fmt.Printf("%.2f\t'%v'\n", running, k)
+		} else {
+			break
+		}
+	}
+
+	// combine common words together
+	cur := 0
+	max := 0
+	prefix := []string{}
+	words := meta.Words
+	//words := []string{}
+	single := map[int]string{}
+	if false {
+		for _, w := range meta.Words {
+			if _, ok := common[w]; ok {
+				cur++
+				prefix = append(prefix, w)
+			} else {
+				if max < cur {
+					max = cur
+				}
+				if cur > 1 {
+					words = append(words, strings.Join(prefix, ""))
+				}
+				words = append(words, w)
+				prefix = []string{}
+				cur = 0
+			}
+		}
+	}
+
+	nodes := map[string]map[string]int{}
+	prev := ""
+	unique := map[string]int{}
+	for i, word := range words {
+		// split out single use words
+		if meta.Count[word] == 1 {
+			single[i] = word
+			continue
+		}
+		// count how many times this word shows up
+		unique[word]++
+
+		// establish word order relations
+		next, ok := nodes[prev]
+		if !ok {
+			next = map[string]int{}
+			nodes[prev] = next
+		}
+		// count how many times this relation exists
+		next[word]++
+		prev = word
+	}
+
+	// generate list of unique keys
+	uniqueKeys := []string{}
+	for k := range unique {
+		uniqueKeys = append(uniqueKeys, k)
+	}
+
+	// sort the keys according to count
+	slices.SortStableFunc(uniqueKeys, func(a, b string) int {
+		if unique[a] != unique[b] {
+			return cmp.Compare(unique[a], unique[b])
+		}
+		return cmp.Compare(a, b)
+	})
+	slices.Reverse(uniqueKeys)
+
+	sortOrder := map[string]int{}
+	sortMapping := map[int]string{}
+	for i, w := range uniqueKeys {
+		sortOrder[w] = i
+		sortMapping[i] = w
+	}
+	fmt.Println("")
+	length := 0
+	ruleDist := map[int]int{}
+	for _, prev := range uniqueKeys {
+		node := nodes[prev]
+		mult := []int{}
+		single := []int{}
+
+		for key, count := range node {
+			ruleDist[count]++
+			if count == 1 {
+				single = append(single, sortOrder[key])
+			} else {
+				mult = append(mult, sortOrder[key])
+			}
+		}
+		buff := []byte{}
+		buff = binary.AppendUvarint(buff, uint64(len(prev)))
+		buff = append(buff, []byte(prev)...)
+		buff = binary.AppendUvarint(buff, uint64(len(mult)))
+		buff = binary.AppendUvarint(buff, uint64(len(single)))
+		slices.Sort(single)
+		slices.Sort(mult)
+
+		p := 0
+		for _, id := range mult {
+			delta := id - p
+			p = id
+			buff = binary.AppendUvarint(buff, uint64(delta))
+		}
+		p = 0
+		for _, id := range single {
+			delta := id - p
+			p = id
+			buff = binary.AppendUvarint(buff, uint64(delta))
+		}
+
+		length += len(buff)
+
+	}
+	//fmt.Println(ruleDist)
+	p := 0
+	deltas := map[int]int{}
+	k := []int{}
+	for pos := range single {
+		k = append(k, pos)
+	}
+
+	slices.Sort(k)
+	skip := []byte{}
+	strings := []byte{}
+	for _, pos := range k {
+		delta := pos - p - 1 // we want how many to skip
+		skip = binary.AppendUvarint(skip, uint64(delta))
+		strings = binary.AppendUvarint(strings, uint64(len(single[pos])))
+		strings = append(strings, []byte(single[pos])...)
+		p = pos
+		deltas[delta]++
+	}
+	//fmt.Println(deltas)
+
+	ruleSplit := map[string]int{}
+	// we need to setup a mapping so that these nodes know which index in the rules they are
+	for prev, node := range nodes {
+		single := []string{}
+		mult := []string{}
+		for key, count := range node {
+			if count == 1 {
+				single = append(single, key)
+			} else {
+				mult = append(mult, key)
+			}
+		}
+		// basic sorting
+		//	slices.Sort(single)
+		//	slices.Sort(mult)
+		// sort the keys according to count (a and b are switched to do acending order)
+		keySort := func(a, b string) int {
+			return cmp.Compare(sortOrder[a], sortOrder[b])
+		}
+		slices.SortStableFunc(single, keySort)
+		slices.SortStableFunc(mult, keySort)
+		//fmt.Println(prev, mult, single)
+		keys := append(mult, single...)
+		ruleSplit[prev] = len(mult)
+
+		// set the index of the key into the node
+		node = map[string]int{}
+		for i, key := range keys {
+			node[key] = i
+		}
+		nodes[prev] = node
+	}
+
+	// build the bit stream that represents index in graph
+	ones := []byte{} // encode 1==0 and 1 means move up
+	//	onesIdx := byte(0)
+	//cOnes := byte(0)
+
+	four := []byte{} // 2==0,3==1,4==2 and 3 means move up
+	//	fourIdx := byte(0)
+	//cFour := byte(0)
+
+	twenty := []byte{} // 20 means move up
+	//twentyIdx := byte(0)
+	//cTwenty := byte(0)
+
+	bytes := []byte{} // whole byte encoding
+	rest := []byte{}  // just normal UVarint encoding
+
+	prevWord := ""
+	//fmt.Println(nodes)
+	idxDist := map[int]int{}
+	//fmt.Println(ruleSplit)
+	skipped := 0
+	//encoded := map[int]int{}
+	for _, word := range words {
+		if meta.Count[word] == 1 {
+			// skip unique words
+			continue
+		}
+		if prevWord == "" {
+			// first time we don't need to encode
+			prevWord = word
+			continue
+		}
+		node := nodes[prevWord]
+		idx := node[word]
+		prevWord = word
+		if ruleSplit[prevWord] <= idx {
+			skipped++
+			//continue
+		}
+		idxDist[idx]++
+		fmt.Println(prevWord, len(node))
+
+		rest = binary.AppendUvarint(rest, uint64(idx))
+		continue
+		/*
+			onesIdx++
+			if idx == 1 {
+				encoded[1]++
+				cOnes |= byte(1 << (onesIdx - 1))
+				if onesIdx == 8 {
+					onesIdx = 0
+					ones = append(ones, cOnes)
+					cOnes = 0
+				}
+				continue
+			}
+			if onesIdx == 8 {
+				onesIdx = 0
+				// set the last bit
+				cFour |= 0x80
+				ones = append(ones, cOnes)
+				cOnes = 0
+			}
+
+			// we encoded 1, so we remove it
+			idx--
+
+			fourIdx += 2
+			if idx < 3 {
+				encoded[4]++
+				cFour |= (byte(idx) << (fourIdx - 2))
+				if fourIdx == 8 {
+					fourIdx = 0
+					four = append(four, cFour)
+					cFour = 0
+				}
+				continue
+			}
+			if fourIdx == 8 {
+				fourIdx = 0
+				// set the last two bits
+				cFour |= 0xc0
+				four = append(four, cFour)
+				cFour = 0
+			}
+
+			// we encoded 3, so we remove them
+			idx -= 3
+
+			twentyIdx += 4
+			if idx < 15 {
+				encoded[20]++
+				cTwenty |= (byte(idx) << (twentyIdx - 4))
+				if twentyIdx == 8 {
+					twentyIdx = 0
+					twenty = append(twenty, cTwenty)
+					cTwenty = 0
+				}
+				continue
+			}
+			if twentyIdx == 8 {
+				twentyIdx = 0
+				// set the last four bits
+				cTwenty |= 0xf0
+				twenty = append(twenty, cTwenty)
+				cTwenty = 0
+			}
+
+			// we encoded 15, so we remove them
+			idx -= 15
+
+			if idx < 255 {
+				encoded[255]++
+				bytes = append(bytes, byte(idx))
+				continue
+			}
+			bytes = append(bytes, byte(0xff))
+
+			// we encoded 255, so we remove them
+			idx -= 255
+
+			encoded[9999]++
+			rest = binary.AppendUvarint(rest, uint64(idx))
+		*/
+	}
+	fmt.Println(idxDist)
+
+	fmt.Println("skipped rules", skipped)
+	fmt.Println("original tokens", len(meta.Unique))
+	fmt.Println("total tokens", len(nodes))
+	fmt.Println("tokens used once", len(single))
+	fmt.Println("total words", len(words), float32(len(words))/float32(meta.Length)*100)
+	fmt.Println("total bytes without key", length, float32(length)/float32(meta.Length)*100)
+	fmt.Println("single use token size", len(skip), len(strings), float32(len(skip)+len(strings))/float32(meta.Length)*100)
+	fmt.Println("total file bytes", meta.Length)
+	fmt.Println("wierd encoding")
+	fmt.Println("ones", meta.percent(len(ones)))
+	fmt.Println("four", meta.percent(len(four)))
+	fmt.Println("twenty", meta.percent(len(twenty)))
+	fmt.Println("byte", meta.percent(len(bytes)))
+	fmt.Println("rest", meta.percent(len(rest)))
+	fmt.Println("total", meta.percent(len(twenty)+len(ones)+len(four)+len(rest)+len(bytes)))
+	fmt.Println("combined total", meta.percent(len(twenty)+len(ones)+len(four)+len(rest)+len(bytes)+len(skip)+len(strings)+length))
+	testCompression(rest)
+}
+
+func (f *file) percent(val int) float32 {
+	return float32(val) / float32(f.Length) * 100
 }
